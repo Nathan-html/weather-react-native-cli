f7ea44a6e599ee27fa86a85711a6b557038717c4
Nathan Flacher <nathan.flacher.pro@gmail.com>
2023-02-22 01:13:53 +0100

    add loader, librabry plus a litlle bit of style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,36 +1,54 @@
-import { Text, View } from "react-native";
-import requestWeatherAPI from "./request";
+import { ActivityIndicator, Text, View } from "react-native";
 import { useEffect, useState } from "react";
 import styles from "../../style/style";
 import { API_WEATHER_TOKEN } from "@env";
+import { getWeatherData } from "weather-api.ts";
+import { ResponseWeatherAPI } from "weather-api.ts/lib/types";
 
 function Home (): JSX.Element {
-    const [data, setData] = useState<ResponseWeatherAPI | undefined>(undefined);
-    const getWeatherData = () => {
-        requestWeatherAPI({key: API_WEATHER_TOKEN, q: 'auto', lang: 'fr' })
-            .then((res: ResponseWeatherAPI) => {
-                setData(res);
-            })
-            .catch((error: Error) => {
-                console.error(error);
-            }) 
-    }
+    const [data, setData] = useState<ResponseWeatherAPI | undefined>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const lang: string = 'fr';
+    const query: string = 'cremieu';
     useEffect(() => {
-        getWeatherData();
-    }, [])
-    return <View>
-        <View style={{
-            margin: 64
-            }}>
-            <Text style={styles.localisation}>{data?.location.name}</Text>
-            <View style={styles.currentTempContainer}>
-                <Text style={styles.currentTemp}>{data?.current.temp_c}<Text  style={styles.tempType}>°C</Text></Text>
-            </View>
-            <Text style={styles.feelsLike}>{data?.current.feelslike_c}</Text>
-            <Text style={styles.conditionText}>{data?.current.condition.text}</Text>
-        </View>
+        getWeatherData({key: API_WEATHER_TOKEN, q: query, lang: lang })
+            .then(response => {
+                console.log('response', response);
+                setData(response);
+                setLoading(false);
+                if (response.status === 200) {
 
-    </View>
+                } else {
+
+                }
+            }).catch(error => {
+                console.error(error);
+            });
+    }, [])
+
+    if (loading) {
+        return <View style={styles.loading}>
+            <ActivityIndicator size="large" />
+            <Text>Loading</Text>
+        </View>
+    } else {
+        return <View>
+            <View style={{
+                margin: 64
+                }}>
+                <View style={styles.header}>
+                    <Text style={styles.localisation}>{data?.location?.name}</Text>
+                    <Text style={styles.localtime}>{data?.location && new Date(data?.location?.localtime).toLocaleString(lang)}</Text>
+                </View>
+                <View style={styles.currentTempContainer}>
+                    <Text style={styles.currentTemp}>{data?.current?.temp_c}</Text>
+                    <Text style={styles.tempType}>°C</Text>
+                </View>
+                <Text style={styles.feelsLike}>{data?.current?.feelslike_c}</Text>
+                <Text style={styles.conditionText}>{data?.current?.condition.text}</Text>
+            </View>
+        </View>
+    }
 }
 
 export default Home;
--- src/style/style.ts
+++ src/style/style.ts
@@ -1,28 +1,37 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
+    loading: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    localtime: {
+        fontSize: 10
+    },
     localisation: {
-        textAlign: "center",
-        fontSize: 32
+        fontSize: 24
     },
     currentTempContainer: {
-        justifyContent: "center"
+        flexDirection: "row"
     },
     currentTemp: {
-        textAlign: "center",
-        fontSize: 64,
+        fontSize: 124,
         verticalAlign: 'top'
     },
-    feelsLike: {
-        textAlign: "center",
-        fontSize: 20
-    },
     tempType: {
+        paddingTop: 32,
         fontSize: 24
+    },
+    feelsLike: {
+        fontSize: 20
     },
     conditionText: {
         fontSize: 20,
-        textAlign: "center",
     }
 });
 
